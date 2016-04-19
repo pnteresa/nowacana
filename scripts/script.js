bffba51ec0d1fae60cdd12f9421b8663c34cfcda
Teresa Pranyoto <teresapranyoto@gmail.com>
2016-04-19 12:55:09 +0700

    moved js, changed textbox width

---
+++ scripts/script.js
@@ -0,0 +1,42 @@
+var typingTimer;
+var deleteTimer;
+var doneTypingInterval = 10;
+var finaldoneTypingInterval = 500;
+var deleteTimeInterval = 2000;
+var typed = true;
+var deleter = [];
+
+var oldData = $("p.typingStatus").html();
+$('#textBox').keydown(function() {
+  typed = true;
+  for (var i = 1; i < deleter.length; i++) {
+    clearTimeout(deleter[i]);
+  };
+  clearTimeout(typingTimer);
+  clearTimeout(deleteTimer);
+  if ($('#textBox').val) {
+    typingTimer = setTimeout(function() {
+      $("p.typingStatus").html("typing...");
+    }, doneTypingInterval);
+  }
+});
+
+$('#textBox').keyup(function() {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(function() {
+    $("p.typingStatus").html("deleting...");
+  }, finaldoneTypingInterval);
+
+  clearTimeout(deleteTimer);
+    typed = false;
+
+  deleteTimer = setTimeout(function() {
+    var txt = $("#textBox");
+    for (var j = 1; j < txt.val().length; j++) {
+      if (typed) break;
+      deleter[j] = setTimeout(function() {
+        if (!typed && txt.val().length > 0) txt.val(txt.val().slice(0,-1));
+      }, j*500);
+    }
+  }, deleteTimeInterval);
+});
